443fbe0b9db410bc4c6d8196026fba52038d0e99
alieyuib <alieyuib@gmail.com>
2022-07-06 23:51:03 +0100

    dashboard-header

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $data_view['page-header'] = 'School Administrator Dashboard';
         return view('dashboard.index');
     }
 }
